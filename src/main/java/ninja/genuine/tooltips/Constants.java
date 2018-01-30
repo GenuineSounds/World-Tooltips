4bb3500e13e0087ab6a78483037a50c91c9aa5e1
GenuineSounds <baw.mls@gmail.com>
2018-01-30 02:23:57 -0800

    VERSION 1.2.4 -> 1.2.5

--- src/main/java/ninja/genuine/tooltips/Constants.java
+++ src/main/java/ninja/genuine/tooltips/Constants.java
@@ -6,5 +6,5 @@ public class Constants {
 	public static final String NAME = "World-Tooltips";
 	public static final String GUIID = "worldtooltipsgui";
 	public static final String URL = "http://genuine.ninja/world-tooltips/";
-	public static final String VERSION = "1.2.4";
+	public static final String VERSION = "1.2.5";
 }
